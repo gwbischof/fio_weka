08c7bb8ae254a6bb7c427f7fe0f439756febf929
EC2 Default User <ec2-user@ip-172-31-50-13.us-west-2.compute.internal>
2020-04-19 08:19:32 +0000

    updates to manual setup

--- manual.sh
+++ manual.sh
@@ -3,5 +3,5 @@
 # I've defined 6 diff "det" sizes
 # for each client we'll randomly assign one...
 date
-time cat privateips | parallel 'bash -c "jobs=($(sed -nr '\''/^\[([0-9].*-(detector|hdf5))-prelayout\].*$/{s||\1|p}'\'' fio-jobfiles/026-detector_prelayout.job)); rnd=\$((RANDOM % 6)); ./fio/fio --client={} fio-jobfiles/025-\${jobs[\$rnd]}_read_write.job \$rnd"'
+time cat privateips | parallel 'bash -c "jobs=($(sed -nr '\''/^\[([0-9].*-(detector|hdf5))-prelayout\].*$/{s||\1|p}'\'' fio-jobfiles/026-detector_prelayout.job)); rnd=\$((RANDOM % 6)); ./fio/fio --client={} fio-jobfiles/025-\${jobs[\$rnd]}_read_write.job"'
 date
